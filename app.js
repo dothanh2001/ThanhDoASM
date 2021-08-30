c3f088d54c3aa9e230406f800a66e45d2947e601
Do Cong Thanh <thanhdcgch190465@fpt.edu.vn>
2021-08-30 21:55:57 +0700

    ok

--- app.js
+++ app.js
@@ -16,17 +16,12 @@ app.use(express.static('public'))
 app.post('/add',async(req,res)=>{
        const nameInput = req.body.txtName;
        const priceInput = req.body.txtPrice;
-       if (priceInput %2 != 0){
-           res.render("index",{errorMsg:'gia nhap vao phai la so chan'})
-           return;
-       }
        const newProduct = {Name: nameInput, Price : priceInput}
        const client = await MongoClient.connect(url);
        const dbo = client.db("ThanhDoDB");
        await dbo.collection("products").insertOne(newProduct);
        res.redirect("/");
 })
-
 app.get('/delete',async (req,res)=>{
     const id  =req.query.id;
     const client = await MongoClient.connect(url);
@@ -41,19 +36,18 @@ app.get('/',async (req,res)=>{
     res.render('index',{data:allProducts})
    // return dbo;
 })
-
-app.get('/edit', async (req, res) => {
-    const idInput = req.query.id;
-    const search = await getProductById(idInput);
-    res.render('edit', {products: search});
-})
-
 app.post('/update', async (req, res) => {
     const id = req.body.id;
     const nameInput = req.body.txtName
     const priceInput = req.body.txtPrice
     await updateProductById(id, nameInput,priceInput);
     res.redirect('/');
+})
+
+app.get('/edit', async (req, res) => {
+    const idInput = req.query.id;
+    const search = await getProductById(idInput);
+    res.render('edit', {products: search});
 })
 
 async function getProductById(idInput){
